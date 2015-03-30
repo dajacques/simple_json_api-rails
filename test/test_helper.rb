8b74b916bfd2835a1a601310b33d35f7e992fc21
Gary Gordon <gfgordon@gmail.com>
2015-03-30 10:57:01 -0400

    Fix fixture path

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,23 +1,20 @@
-# Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
 
 require 'minitest/spec'
-
 require File.expand_path('../../test/dummy/config/environment.rb',  __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path('../../test/dummy/db/migrate', __FILE__)]
-require 'rails/test_help'
 
-# Filter out Minitest backtrace while allowing backtrace from other libraries
-# to be shown.
+require 'rails/test_help'
+require 'minitest/rails'
+
+ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 
 # Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+# Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 class ActiveSupport::TestCase
   fixtures :all
 end
-
-require 'minitest/rails'
 
 require 'simple_json_api/rails'
