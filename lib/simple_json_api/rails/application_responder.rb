924bbfab55300c55ac1994e79d0e5ee18c68953e
Gary Gordon <gfgordon@gmail.com>
2015-04-13 15:36:04 -0400

    Don't use responders

--- lib/simple_json_api/rails/application_responder.rb
+++
@@ -1,9 +0,0 @@
-require 'simple_json_api/rails/responders/json_api_responder'
-
-module SimpleJsonApi
-  module Rails
-    class ApplicationResponder < ActionController::Responder
-      include Responders::JsonApiResponder
-    end
-  end
-end
--- simple_json_api-rails.gemspec
+++ simple_json_api-rails.gemspec
@@ -16,5 +16,4 @@ Gem::Specification.new do |s|
 
   s.add_dependency 'simple_json_api', '~> 0.0.4'
   s.add_dependency 'rails', '>= 4.1'
-  s.add_dependency 'responders'
 end
